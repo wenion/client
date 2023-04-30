1b958da40385144d41051c44ff371fadcac147c6
jim wen <jim.wen@monash.edu>
2023-04-30 22:46:02 +1000

    fix - query page submit logic & UI for query page

--- src/site/components/TopBar.tsx
+++ src/site/components/TopBar.tsx
@@ -2,17 +2,16 @@ import {
   LinkButton,
   SearchIcon,
 } from '@hypothesis/frontend-shared/lib/next';
-import { useRef, useState } from 'preact/hooks';
+import { useEffect, useRef } from 'preact/hooks';
 
 import type { SidebarSettings } from '../../types/config';
-import { isThirdPartyService } from '../../sidebar/helpers/is-third-party-service';
 import { applyTheme } from '../../sidebar/helpers/theme';
 import { withServices } from '../../sidebar/service-context';
 import type { QueryService } from '../../sidebar/services/query';
 import type { FrameSyncService } from '../../sidebar/services/frame-sync';
 import { useSidebarStore } from '../../sidebar/store';
 import UserMenu from './UserMenu';
-import LogoIcon from '../static/logo';
+import LogoIcon from '../static/logo-monash';
 
 export type TopBarProps = {
   /** Flag indicating whether the app is in a sidebar context */
@@ -46,84 +45,52 @@ function TopBar({
   queryService,
   settings,
 }: TopBarProps) {
-  const showSharePageButton = !isThirdPartyService(settings);
   const loginLinkStyle = applyTheme(['accentColor'], settings);
 
   const store = useSidebarStore();
-  const filterQuery = store.filterQuery();
   const isLoggedIn = store.isLoggedIn();
   const hasFetchedProfile = store.hasFetchedProfile();
-  // const input = useRef<HTMLInputElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  // The active filter query from the previous render.
-  // const [prevQuery, setPrevQuery] = useState(store.queryingWord());
-
-  // The query that the user is currently typing, but may not yet have applied.
-  const [pendingQuery, setPendingQuery] = useState(store.queryingWord());
-
-  const onSubmit = (e: Event) => {
-    e.preventDefault();
-    queryService.queryActivity(pendingQuery);
-  };
-
-  // When the active query changes outside of this component, update the input
-  // field to match. This happens when clearing the current filter for example.
-  // if (store.queryingWord() !== prevQuery) {
-  //   setPendingQuery(store.queryingWord());
-  //   setPrevQuery(store.queryingWord());
-  // }
-
-  const onInput = (e: Event) => {
-    const value = (e.target as HTMLInputElement).value;
-    setPendingQuery(value);
-  }
+  const param = window.location.search.match(/[\?&]q=([^&]+)/);
+  useEffect(() => {
+    if (param) {
+      const el = inputRef.current!;
+      el.value = param[1];
+      queryService.queryActivity(param[1]);
+    }
+  }, [param]);
 
   return (
     <div>
       <header class="nav-bar">
-        <div class="nav-bar__content">
-          <a href="https://colam.kmass.cloud.edu.au/" title="Hypothesis homepage" class="nav-bar__logo-container">
+        <div class="nav-bar__content justify-center">
+          <a href="https://colam.kmass.cloud.edu.au/" title="CoLAM homepage" class="nav-bar__logo-container mx-12">
             <LogoIcon />
           </a>
-          <div class="nav-bar__search js-search-bar" data-ref="searchBar">
-            <form action="http://localhost:5000/api/query"
+          <div class="max-w-md nav-bar__search">
+            <form action="/query"
                   class="search-bar"
-                  data-ref="searchBarForm"
                   id="search-bar"
-                  role="search"
-                  onSubmit={onSubmit}
                   >
-
-              {/* <input type="submit" class="nav-bar__search-hidden-input"/> */}
-
-
-              <div class="search-bar__lozenges" data-ref="searchBarLozenges">
-
-                <input class="search-bar__input js-input-autofocus"
+                <input class="search-bar__input"
                       aria-autocomplete="list"
                       aria-label=""
                       aria-haspopup="true"
                       autocapitalize="off"
                       autocomplete="off"
-                      data-ref="searchBarInput"
+                      type="text"
+                      ref={inputRef}
                       name="q"
                       placeholder="Search…"
                       role="combobox"
-                      value={pendingQuery || ''}
-                      onInput={onInput}
                       />
-                <div>
-                  <input type="submit" class="nav-bar__search-hidden-input"/>
-                  <div className="search-bar__icon" onClick={onSubmit}>
-                    <SearchIcon />
-                  </div>
-                </div>
-              </div>
+              <button className="search-bar__icon" type="submit">
+                <SearchIcon />
+              </button>
             </form>
           </div>
-
-          <div class="u-stretch"></div>
-          <nav className="nav-bar-links">
+          <nav className="nav-bar-links mx-14">
             {isLoggedIn ? (
               <UserMenu onLogout={onLogout} />
             ) : (
@@ -154,7 +121,6 @@ function TopBar({
               </div>
             )}
           </nav>
-
         </div>
       </header>
     </div>
