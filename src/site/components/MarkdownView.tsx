384e1eb08e0b0d040b349de8e05a8b582190eae7
jim wen <jim.wen@monash.edu>
2023-05-12 03:11:52 +1000

    dev - add markdown using slowdown

---
+++ src/site/components/MarkdownView.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useMemo, useRef } from 'preact/hooks';
+
+import { replaceLinksWithEmbeds } from '../media-embedder';
+import { renderMathAndMarkdown } from '../render-markdown';
+import StyledText from './StyledText';
+
+export type MarkdownViewProps = {
+  /** The string of markdown to display as HTML. */
+  markdown: string;
+  classes?: string;
+  style?: Record<string, string>;
+};
+
+/**
+ * A component which renders markdown as HTML and replaces recognized links
+ * with embedded video/audio.
+ */
+export default function MarkdownView({
+  markdown,
+  classes,
+  style,
+}: MarkdownViewProps) {
+  const html = useMemo(
+    () => (markdown ? renderMathAndMarkdown(markdown) : ''),
+    [markdown]
+  );
+  const content = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    replaceLinksWithEmbeds(content.current!, {
+      // Make embeds the full width of the sidebar, unless the sidebar has been
+      // made wider than the `md` breakpoint. In that case, restrict width
+      // to 380px.
+      className: 'w-full md:w-[380px]',
+    });
+  }, [markdown]);
+
+  // NB: The following could be implemented by setting attribute props directly
+  // on `StyledText` (which renders a `div` itself), versus introducing a child
+  // `div` as is done here. However, in initial testing, this interfered with
+  // some overflow calculations in the `Excerpt` element. This could be worth
+  // a review in the future.
+  return (
+    <div className="w-full break-words cursor-text">
+      <StyledText>
+        <div
+          className={classes}
+          data-testid="markdown-text"
+          ref={content}
+          dangerouslySetInnerHTML={{ __html: html }}
+          style={style}
+        />
+      </StyledText>
+    </div>
+  );
+}
