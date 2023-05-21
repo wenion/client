94f95e4588dc1bf19615fdef529f22b3d3ac8d32
jim wen <jim.wen@monash.edu>
2023-05-21 14:32:23 +1000

    fix - foggot submit site-sidebar event

---
+++ src/types/site-port-rpc-events.d.ts
@@ -0,0 +1,22 @@
+/**
+ * This module defines the events that are sent between frames with different
+ * roles in the client (guest, host, sidebar).
+ */
+
+/**
+ * Events that the guest sends to the sidebar
+ */
+export type SiteToSidebarEvent =
+  /**
+   * The sidebar is asking the guest(s) to delete an annotation.
+   */
+  | 'test';
+
+/**
+ * Events that the sidebar sends to the guest(s)
+ */
+export type SidebarToSiteEvent =
+  /**
+   * Show a banner with information about the current content.
+   */
+  | 'updateProfile';
