bf3ad3509b485144bb205200370aee468ac6ae8d
jim wen <jim.wen@monash.edu>
2024-01-23 11:03:25 +1100

    dev - timer demo

--- src/sidebar/index.tsx
+++ src/sidebar/index.tsx
@@ -41,6 +41,7 @@ import { ThreadsService } from './services/threads';
 import { ToastMessengerService } from './services/toast-messenger';
 import { QueryService } from './services/query';
 import { VideoAnnotationsService } from './services/video-annotations';
+import { TimerService } from './services/timer';
 import { createSidebarStore } from './store';
 import type { SidebarStore } from './store';
 import { disableOpenerForExternalLinks } from './util/disable-opener-for-external-links';
@@ -99,10 +100,12 @@ function initServices(
   autosaveService: AutosaveService,
   persistedDefaults: PersistedDefaultsService,
   serviceURL: ServiceURLService,
+  timerService: TimerService,
 ) {
   autosaveService.init();
   persistedDefaults.init();
   serviceURL.init();
+  timerService.init();
 }
 
 /**
@@ -161,6 +164,7 @@ function startApp(settings: SidebarSettings, appEl: HTMLElement) {
     .register('toastMessenger', ToastMessengerService)
     .register('queryService', QueryService)
     .register('videoAnnotationsService', VideoAnnotationsService)
+    .register('timerService', TimerService)
     .register('store', { factory: createSidebarStore });
 
   // Register utility values/classes.
