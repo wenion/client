003dd598bff4acb614eaf6f0f1c88aa86883c9cf
jim wen <jim.wen@monash.edu>
2024-08-23 05:22:29 +1000

    hotfix: update the clicking for sharing---it is always hard to click the button

--- src/sidebar/components/RecordingList.tsx
+++ src/sidebar/components/RecordingList.tsx
@@ -4,13 +4,9 @@ import classnames from 'classnames';
 
 import { withServices } from '../service-context';
 import { useSidebarStore } from '../store';
-import ShareIcon from '../../images/icons/shared';
-import UnshareIcon from '../../images/icons/unshared';
 import type { RecordingService } from '../services/recording';
+import RecordingMenu from './RecordingMenu';
 
-import Menu from './Menu';
-import MenuItem from './MenuItem';
-import MenuSection from './MenuSection';
 
 const capitalize = (word: string) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
@@ -36,13 +32,6 @@ function RecordingList({
   const recordings = store.Records();
   const userid = store.profile().userid;
 
-  
-  const menuLabel = (
-    <div className="rotate-90">
-      <EllipsisIcon />
-    </div>
-  );
-
   return (
     <>
       <h1 className='m-4 text-xl'>Saved ShareFlow (Comic)</h1>
@@ -52,10 +41,14 @@ function RecordingList({
       >
         <TableBody>
           {recordings.map(child => (
-            <TableRow
-              onClick={(event) => {event.stopPropagation(); recordingService.getRecording(child.sessionId, child.userid);}}
+            <TableRow>
+              <div
+                className={classnames('flex')}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  recordingService.getRecording(child.sessionId, child.userid);
+                }}
               >
-              <div className={classnames('flex')}>
                 <div id="shareflow" className="grow text-lg items-center flex gap-x-2">
                   <PreviewIcon/>
                   {child.taskName}
@@ -77,42 +70,26 @@ function RecordingList({
                     </div>
                   </div>
                 )}
-                <div
-                  className={classnames(
-                    'flex items-center font-semibold rounded',
-                    'text-grey-7 bg-grey-1',
-                    'enabled:hover:text-grey-9 enabled:hover:bg-grey-2',
-                    'aria-pressed:text-grey-9 aria-expanded:text-grey-9',
-                    'p-2 gap-x-2 grow-0 m-1 bg-grey-0 hover:bg-blue-400',
-                  )}>
-                  <Menu
-                    label={menuLabel}
-                    title='More actions'
-                    align="right"
-                    menuIndicator={false}
+                {userid && child.userid && userid === child.userid ? (
+                  <RecordingMenu
+                    recording={child}
+                  />
+                ) : (
+                  <div
+                    className={classnames(
+                      'flex items-center font-semibold rounded',
+                      'text-grey-7 bg-grey-1',
+                      'enabled:hover:text-grey-9 enabled:hover:bg-grey-2',
+                      'aria-pressed:text-grey-9 aria-expanded:text-grey-9',
+                      'grow-0 m-1 bg-grey-0 hover:bg-blue-400',
+                    )}
+                    onClick={(event) => {event.stopPropagation();}}
                   >
-                    <MenuSection>
-                      {userid && child.userid && userid === child.userid && (
-                        <>
-                          <MenuItem
-                            label={!!child.shared? 'Unshare' : 'Share'}
-                            icon={!!child.shared? UnshareIcon: ShareIcon}
-                            onClick={() => {
-                              recordingService.updateRecording(child.session_id, !child.shared);
-                            }}
-                          />
-                          <MenuItem
-                            label='Delete'
-                            icon={TrashIcon}
-                            onClick={() => {
-                              store.selectRecordBySessionId(child.sessionId, 'delete')
-                            }}
-                          />
-                        </>
-                      )}
-                    </MenuSection>
-                  </Menu>
-                </div>
+                    <span className="rotate-90 p-2">
+                      <EllipsisIcon />
+                    </span>
+                  </div>
+                )}
               </div>
             </TableRow>
           ))}
---
+++ src/sidebar/components/RecordingMenu.tsx
@@ -0,0 +1,72 @@
+import { TrashIcon, EllipsisIcon } from '@hypothesis/frontend-shared';
+import { useState } from 'preact/hooks';
+import classnames from 'classnames';
+
+import { withServices } from '../service-context';
+import { useSidebarStore } from '../store';
+import ShareIcon from '../../images/icons/shared';
+import UnshareIcon from '../../images/icons/unshared';
+import type { RecordingService } from '../services/recording';
+import type { Recording } from '../../types/api';
+
+import Menu from './Menu';
+import MenuItem from './MenuItem';
+import MenuSection from './MenuSection';
+
+
+export type RecordingMenuProps = {
+  recordingService: RecordingService;
+  recording: Recording;
+};
+
+function RecordingMenu({
+  recordingService,
+  recording,
+}: RecordingMenuProps) {
+  const store = useSidebarStore();
+  const [isOpen, setOpen] = useState(false);
+
+  const menuLabel = (
+    <span className="rotate-90 p-2">
+      <EllipsisIcon />
+    </span>
+  );
+  return (
+    <div
+      className={classnames(
+        'flex items-center font-semibold rounded',
+        'text-grey-7 bg-grey-1',
+        'enabled:hover:text-grey-9 enabled:hover:bg-grey-2',
+        'aria-pressed:text-grey-9 aria-expanded:text-grey-9',
+        'grow-0 m-1 bg-grey-0 hover:bg-blue-400',
+      )}>
+      <Menu
+        label={menuLabel}
+        title='More actions'
+        align="right"
+        menuIndicator={false}
+        open={isOpen}
+        onOpenChanged={setOpen}
+      >
+        <MenuSection>
+          <MenuItem
+            label={!!recording.shared? 'Unshare' : 'Share'}
+            icon={!!recording.shared? UnshareIcon: ShareIcon}
+            onClick={() => {
+              recordingService.updateRecording(recording.session_id, !recording.shared);
+            }}
+          />
+          <MenuItem
+            label='Delete'
+            icon={TrashIcon}
+            onClick={() => {
+              store.selectRecordBySessionId(recording.sessionId, 'delete')
+            }}
+          />
+        </MenuSection>
+      </Menu>
+    </div>
+  );
+}
+
+export default withServices(RecordingMenu, ['recordingService']);
