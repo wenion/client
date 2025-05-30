803b2afb83c76bc0a14a1e77b020993907571072
jim wen <jim.wen@monash.edu>
2025-05-31 02:17:33 +1000

    Remove unuseful code

--- src/site/components/ComicsCard.tsx
+++ src/site/components/ComicsCard.tsx
@@ -106,8 +106,6 @@ function Thumbnail({
           'cursor-pointer',
         )}
         id={'img' + trace.id}
-        // onMouseEnter={() => hover(true)}
-        // onMouseLeave={() => hover(false)}
         alt={trace.title}
         src={trace.image!}
         onLoad={onLoad}
