aa4d92c5e9d6016f62e4fca2461a3f3ba6e4dc5d
jim wen <jim.wen@monash.edu>
2025-06-08 18:41:13 +1000

    Comic image circle position

--- src/sidebar/components/ComicCard.tsx
+++ src/sidebar/components/ComicCard.tsx
@@ -35,8 +35,8 @@ function Thumbnail({
   onElementSizeChanged
 }: ThumbnailProps) {
   const [loaded, setLoaded] = useState(false);
-  const [circleTop, setCircleTop] = useState(0);
-  const [circleLeft, setCircleLeft] = useState(0);
+  const [circleTop, setCircleTop] = useState<number | null>(null);
+  const [circleLeft, setCircleLeft] = useState<number | null>(null);
   const imageRef = useRef<HTMLImageElement | null>(null);
   const circleRef = useRef<HTMLDivElement | null>(null);
 
@@ -45,7 +45,7 @@ function Thumbnail({
     const imageHeight = imageRef.current.clientHeight;
     const { width, height, clientX: offsetX, clientY: offsetY } = trace;
 
-    if (width > 0 && height > 0 && offsetX >= 0 && offsetY >= 0) {
+    if (width > 0 && height > 0 && offsetX !== null && offsetY !== null) {
       const widthToHeight = width / height;
       const ratioHeight = imageHeight/height;
       const ratioWidth = widthToHeight * imageHeight / width;
@@ -117,15 +117,17 @@ function Thumbnail({
         src={trace.image!}
         onLoad={() => onLoad(trace.id)}
       />
-      <div
-        ref={circleRef}
-        className={classnames(
-          "w-6 h-6 rounded-full",
-          "absolute border-2 border-red-500 bg-red-100/35 transition-all",
-          "duration-300 ease-in-out",
-        )}
-        style={{ top: `${circleTop}px`, left: `${circleLeft}px` }}
-      />
+      {circleTop && circleLeft && (
+        <div
+          ref={circleRef}
+          className={classnames(
+            "w-6 h-6 rounded-full",
+            "absolute border-2 border-red-500 bg-red-100/35 transition-all",
+            "duration-300 ease-in-out",
+          )}
+          style={{ top: `${circleTop}px`, left: `${circleLeft}px` }}
+        />
+      )}
     </div>
   )
 }
