f7b5e6408b6a61d474e39e6bf8ade0c5d069ee05
jim wen <jim.wen@monash.edu>
2025-07-26 18:36:10 +1000

    Sync last open to Extension

--- src/sidebar/services/frame-sync.ts
+++ src/sidebar/services/frame-sync.ts
@@ -751,6 +751,15 @@ export class FrameSyncService {
       (lastOpen, prevLastOpen) => {
         this._hostRPC.call('setSidebarVisible', lastOpen);
         this._guestRPC.forEach(rpc => rpc.call('setSidebarVisible', lastOpen));
+        this._extensionRPC.call(
+          'customEvent',
+          {
+            eventType: 'client',
+            custom: 'lastOpen',
+            tagName: 'lastOpen',
+            textContent: lastOpen,
+          }
+        );
       }
     );
 
